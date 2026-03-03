c52f8f0ca4741c5fd3954a0230750338d9f85ac7
Jasmine Regnér <jasmine.regner@proton.me>
2026-03-03 08:44:45 +0100

    fix: add inquire crate as dependency.
    Create config command file.

---
+++ src/commands/config.rs
@@ -0,0 +1,10 @@
+/**
+ * @license MIT License
+ * @author Jasmine Regnér
+ */
+use crate::config_creator::{create_config_directory};
+
+
+fn overwrite_config_interactively() {
+    let template_sheet = Text::new("Where do you want to place your sheet templates?").prompt();
+}
---
+++ src/commands/mod.rs
@@ -0,0 +1,6 @@
+/**
+ * @license MIT License
+ * @author Jasmine Regnér
+ */
+
+ mod config;

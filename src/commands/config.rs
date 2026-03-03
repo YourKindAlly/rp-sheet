ee598738e2e700d29eeb37df8f7ee325eb451673
Jasmine Regnér <jasmine.regner@proton.me>
2026-03-03 09:06:40 +0100

    feat: update text prompt.

--- src/commands/config.rs
+++ src/commands/config.rs
@@ -3,8 +3,13 @@
  * @author Jasmine Regnér
  */
 use crate::config_creator::{create_config_directory};
-use inquire::Text;
+use inquire::{InquireError, Text};
+use std::path::Path;
 
-fn overwrite_config_interactively() {
-    let template_sheet = Text::new("Where do you want to place your sheet templates?").prompt();
+fn update_config_interactively() {
+    let template_sheet = Text::new("Which path do you want to put your sheet templates?").prompt();
+}
+
+fn create_input_validator() {
+    
 }
